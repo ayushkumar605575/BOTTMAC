417e4e3eb9745067893c3ca7848653332da11062
ayush <ayushkumar605575@gmail.com>
2024-03-06 00:52:16 +0530

    BOTTMAC

--- app/src/main/java/com/bottmac/bottmac/MainActivity.kt
+++ app/src/main/java/com/bottmac/bottmac/MainActivity.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import android.util.Base64
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -24,7 +36,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import com.bottmac.bottmac.models.ProductItem
-import java.lang.Byte
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 
 @AndroidEntryPoint
@@ -33,19 +45,15 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var api: ProductsApi
     private lateinit var res: List<ProductItem>
-//    @OptIn(DelicateCoroutinesApi::class)
+
+        @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val isComp = GlobalScope.launch {
             val response = api.getProducts()
             if (response.isSuccessful) {
                 res = response.body()!!
-                println(res)
-                println(res[0].productFeatures.split("\\n")[0])
-                println(res[0].productFeatures.split("\\n")[1])
-//                println(res[1])
             }
-//            println(1234567)
         }
         setContent {
             BOTTMACTheme {
@@ -56,74 +64,81 @@ class MainActivity : ComponentActivity() {
                 ) {
                     println(isComp.isCompleted)
                     if (isComp.isCompleted) {
-                    println(isComp)
-//                        println(res[0].productImage[0])
-                        println(21345678)
+//                        val productItem = res
                         LazyColumn {
-                            item { Text(text = res[0].productName) }
-                            item { Text(text = res[0].productFeatures) }
-                            item {
-                                AsyncImage(
-                                        modifier = Modifier.size(400.dp),
-                                        model = ImageRequest.Builder(LocalContext.current)
-                                            .data(Base64.decode(res[0].productImage[0], Base64.DEFAULT))
-                                            .crossfade(500)
-                                            .build(),
-                                        contentDescription = null,
-                                        contentScale = ContentScale.Crop,
-                                        alignment = Alignment.Center
-                                    )
+                            items(res) { productItem ->
+                                ProductCard(
+                                    productName = productItem.productName,
+                                    productsFeatures = productItem.productFeatures.split("\\n"),
+                                    productsImages = productItem.productImage
+                                )
+                                Spacer(modifier = Modifier.height(8.dp))
+                            }
                         }
-//                        }
-//                        LazyColumn {
-//                            item {
-//                                Text(text = res[1].productName, fontSize = 40.sp)
-//                            }
-//                            item {
-//                                LazyRow {
-//                                    items(res[1].productImage.size) { ind ->
-//                                        AsyncImage(
-//                                            modifier = Modifier.size(400.dp),
-//                                            model = ImageRequest.Builder(LocalContext.current)
-//                                                .data(
-//                                                    Base64.decode(
-//                                                        res[1].productImage[ind],
-//                                                        Base64.DEFAULT
-//                                                    )
-//                                                )
-//                                                .crossfade(500)
-//                                                .build(),
-//                                            contentDescription = null,
-//                                            contentScale = ContentScale.Crop,
-//                                            alignment = Alignment.Center
-//                                        )
-//                                    }
-//                                }
-//                            }
-//                        }
                     }
                 }
-//                        LazyColumn {
-//                            items(res.size) {ind->
-//                                LazyRow {
-//                                    item {
-//                                        AsyncImage(
-//                                            modifier = Modifier.size(400.dp),
-//                                            model = ImageRequest.Builder(LocalContext.current)
-//                                                .data(images[ind])
-//                                                .crossfade(500)
-//                                                .build(),
-//                                            contentDescription = null,
-//                                            contentScale = ContentScale.Crop,
-//                                            alignment = Alignment.Center
-//                                    })
-//                                }
-//                            }
-//                        }
             }
         }
     }
 }
+//}
+
+
+@Composable
+fun ProductCard(productName: String, productsFeatures: List<String>, productsImages: List<String>) {
+    ElevatedCard(
+        modifier = Modifier,
+        elevation = CardDefaults.elevatedCardElevation(
+            defaultElevation = 8.dp,
+            pressedElevation = 4.dp
+        )
+    ) {
+        Column {
+            LazyRow {
+                items(productsImages) { image ->
+                    AsyncImage(
+                        modifier = Modifier.size(240.dp),
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(
+                                Base64.decode(
+                                    image,
+                                    Base64.DEFAULT
+                                )
+                            )
+                            .crossfade(500)
+                            .build(),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.Center
+                    )
+                }
+            }
+            Text(text = productName)
+            Text(text = "TECHNICAL SPECIFICATION")
+            for (feature in productsFeatures) {
+                Text(text = " • $feature")
+            }
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                item {
+                    ElevatedButton(onClick = { /*TODO*/ }) {
+                        Text(text = " 📞 Call Now")
+                    }
+                }
+                item {
+                    ElevatedButton(onClick = { /*TODO*/ }) {
+                        Text(text = " Order Now")
+                    }
+                }
+            }
+        }
+    }
 }
 
-
+@Preview(showBackground = true)
+@Composable
+private fun CardPrev() {
+//    ProductCard(productsFeatures = listOf("ayush", "kumar", "Naveen"))
+}
--- app/src/main/java/com/bottmac/bottmac/api/ProductsApi.kt
+++ app/src/main/java/com/bottmac/bottmac/api/ProductsApi.kt
@@ -3,13 +3,12 @@ package com.bottmac.bottmac.api
 import com.bottmac.bottmac.models.ProductItem
 import retrofit2.Response
 import retrofit2.http.GET
-//import retrofit2.http.Header
 
 interface ProductsApi {
 
     @GET("/?format=json")
     suspend fun getProducts(): Response<List<ProductItem>>
 
-    @GET("/?format=json")
-    suspend fun getCategories(): Response<List<String>>
+//    @GET("/?format=json")
+//    suspend fun getCategories(): Response<List<String>>
 }
