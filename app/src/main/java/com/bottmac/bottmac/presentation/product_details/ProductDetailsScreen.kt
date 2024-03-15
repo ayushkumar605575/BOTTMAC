1a49378c43ceaaf61205f1de32b676f927fd8bab
ayush <ayushkumar605575@gmail.com>
2024-03-16 00:22:50 +0530

    Product Details Screen

--- app/src/main/java/com/bottmac/bottmac/presentation/product_details/ProductDetailsScreen.kt
+++ app/src/main/java/com/bottmac/bottmac/presentation/product_details/ProductDetailsScreen.kt
@@ -1,15 +1,7 @@
 package com.bottmac.bottmac.presentation.product_details
 
-import androidx.compose.foundation.gestures.ScrollableDefaults
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun ProductDetailsScreen() {
